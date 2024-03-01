b803bada2b42d8699ac9726d95b92da0f5c8897e
Nycolas <nybertocco@gmail.com>
2024-02-29 22:19:46 -0300

    projeto finalizado

---
+++ main.js
@@ -0,0 +1,23 @@
+const home = document.querySelector(".home")
+const sorte = document.querySelector(".sorte")
+const biscoito = document.querySelector("#biscoito")
+const biscoito2 = document.querySelector("#biscoito-2")
+const frase = document.querySelector("#frase")
+const btnReset = document.querySelector('#btnReset')
+
+    biscoito.addEventListener('click', handleBiscoitoClick)
+    btnReset = document.querySelector('click', handleResetClick)
+
+function handleBiscoitoClick() {
+    toggleScreen()
+}
+
+function handleResetClick() {
+    toggleScreen()
+    sorte.classList.add("hide")
+}
+
+function toggleScreen() {
+    home.classList.toggle("hide")
+    sorte.classList.toggle("hide")
+}
